de01d7df9725e824120bab3629e8c3de3f7cccdb
Tin <krstnpangan@gmail.com>
2018-07-06 17:05:22 +0800

    Redo string

--- celsiustofahrenheit.rb
+++ celsiustofahrenheit.rb
@@ -2,4 +2,4 @@ fahrenheit = (1.8 * celsius) + 32
 celsius = 37
 
 puts "The Celsius value is #{celsius}"
-puts "The corresponding Fahrenheit value is #{fahrenheit}
+puts "The corresponding Fahrenheit value is #{fahrenheit}"
