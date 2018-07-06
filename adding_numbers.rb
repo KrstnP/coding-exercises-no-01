3c33d5cb8d7ada2a2af00a0484c61d8983c5b35e
Tin <krstnpangan@gmail.com>
2018-07-06 16:45:40 +0800

    Created seperate codes for each value

--- adding_numbers.rb
+++ adding_numbers.rb
@@ -1,2 +1,8 @@
 result = 2
-result = result + 4 +6 + 8 +10 + 12 + 14
+result = result + 2
+result = result + 4
+result = result + 6
+result = result + 8
+result = result + 10
+result = result + 12
+result = result + 14
